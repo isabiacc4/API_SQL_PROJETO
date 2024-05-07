e21639f2a042c201379eec7102c9d560647394bb
Rafael12eFaixa <rafael.andriotti8@gmail.com>
2024-05-07 14:13:15 -0300

    meu bgl

---
+++ produtos/Router/produtoRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const produtoController = require('../controllers/produtoController');
+// lembrando que a rota raiz tem clientes, definido no app.js
+// Rota para obter todos os clientes
+router.get('/', produtoController.getAllProdutos);
+// Rota para obter um único cliente pelo ID
+router.get('/:id', produtoController.getProdutoById);
+// Rota para criar um novo cliente
+router.post('/', produtoController.createProduto);
+// Rota para atualizar um cliente existente
+router.put('/:id', produtoController.updateProduto);
+// Rota para deletar um cliente
+router.delete('/:id', produtoController.deleteProduto);
+module.exports = router;
